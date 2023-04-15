eaad4f671a2876e1f28a51af8c3531ac835255c1
shaaleissa <80845124+shaaleissa@users.noreply.github.com>
2023-04-15 06:36:48 +0300

    Svg pic added

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+// hint meme use SvgPicture.asset('filePath') when using the picture
+// bc it's svg so Image.asset('filePath') won't work
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
